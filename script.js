18a351a2573c646ae6d7310dd773ee1fafd4589f
Atharv Shete <be22b021@smail.iitm.ac.in>
2023-10-20 20:05:22 +0530

    some minnor changes

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 
 const buttons = document.querySelectorAll(".button");
-// body.style.backgroundColor = "black"
-console.log(buttons)
-
 
 buttons.forEach((button) => {
-  console.log(button);
-  button.addEventListener('click',(e) => {
-    console.log(e);
-    console.log(e.target);
-    body.style.backgroundColor = e.target.id; 
-  });
+	button.addEventListener("click", (e) => {
+		body.style.backgroundColor = e.target.id;
+	});
 });
-
