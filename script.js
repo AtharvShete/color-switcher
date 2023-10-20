cbe81dbc68dfd38c783f6e3d0060c07ba999ca8a
Atharv Shete <be22b021@smail.iitm.ac.in>
2023-10-20 19:56:21 +0530

    rainbow

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// console.log('Atharv');
+const body = document.querySelector('body');
+
+const buttons = document.querySelectorAll(".button");
+// body.style.backgroundColor = "black"
+console.log(buttons)
+
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    body.style.backgroundColor = e.target.id; 
+  });
+});
+
